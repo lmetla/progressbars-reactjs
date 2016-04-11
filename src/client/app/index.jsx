15c6e36a18d0ec6c343667f45d60acde572d0d19
Lokesh Metla <lmetla@student.unimelb.edu.au>
2016-04-12 00:11:04 +1000

    Initial commit

---
+++ src/client/app/index.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import {render} from 'react-dom';
+
+class App extends React.Component {
+
+  render () {
+    return (
+      <div className ="">
+        <div className ="">
+          <h2> Progressbars using react </h2>
+        </div>
+      </div>
+    );
+  }
+}
+
+render(<App />, document.getElementById('app'));
